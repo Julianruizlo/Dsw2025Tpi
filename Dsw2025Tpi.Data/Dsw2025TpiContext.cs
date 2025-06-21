470f63330dcf6f1fc229cbca1b86878115790931
Lucho <lucianomalica@gmail.com>
2025-06-21 16:46:54 -0300

    feat: agregado clase enum en domain y inMemory,dbContext en data

--- Dsw2025Tpi.Data/Dsw2025TpiContext.cs
+++ Dsw2025Tpi.Data/Dsw2025TpiContext.cs
@@ -1,7 +1,62 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Dsw2025Tpi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dsw2025Tpi.Data;
 
-public class Dsw2025TpiContext: DbContext
+public class Dsw2025TpiContext : DbContext
 {
+
+    public Dsw2025TpiContext(DbContextOptions<Dsw2025TpiContext> options) : base(options)
+    {
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Customer>(eb =>
+        {
+            eb.ToTable("Customers");
+            eb.Property(c => c.Email)
+            .HasMaxLength(320);
+            eb.Property(c => c.Name)
+            .HasMaxLength(60)
+            .IsRequired();
+            eb.Property(c => c.PhoneNumber);
+        });
+        modelBuilder.Entity<Order>(eb =>
+        {
+            eb.ToTable("Orders");
+            eb.Property(o => o.Date)
+            .HasMaxLength(10)
+            .IsRequired();
+            eb.Property(o => o.ShippingAddres)
+            .HasMaxLength(60);
+            eb.Property(o => o.BillingAddress)
+            .HasPrecision(15, 2);
+            eb.Property(o => o.Notes)
+            .HasMaxLength(60);
+        });
+        modelBuilder.Entity<OrderItem>(eb =>
+        {
+            eb.ToTable("OrderItems");
+        });
+        modelBuilder.Entity<Product>(eb =>
+        {
+            eb.ToTable("Products");
+            eb.Property(p => p.Sku)
+            .HasMaxLength(20)
+            .IsRequired();
+            eb.Property(p => p.Name)
+            .HasMaxLength(60)
+            .IsRequired();
+            eb.Property(p => p.CurrentUnitPrice)
+            .HasPrecision(15, 2);
+            eb.Property(p => p.InternalCode)
+            .HasMaxLength(60);
+            eb.Property(p => p.Description)
+            .HasMaxLength(200);
+            eb.Property(p => p.StockQuantity)
+            .HasDefaultValue(0);
+        });
+    }
 }
--- Dsw2025Tpi.Domain/Entities/Product.cs
+++ Dsw2025Tpi.Domain/Entities/Product.cs
@@ -6,31 +6,44 @@ using System.Threading.Tasks;
 
 namespace Dsw2025Tpi.Domain.Entities;
 
- 
+
 public class Product : EntityBase
 {
-
     public Product(string sku, string internalCode, string name, string description, decimal currentUnitPrice, int stockQuantity, bool isAtive)
-        {
-
-          
-            Sku = sku;
-            InternalCode = internalCode;
-            Name = name;
-            Description = description;
-            CurrentUnitPrice = currentUnitPrice;
-            StockQuantity = stockQuantity;
-            IsAtive = isAtive;
-        }
-    public string? Sku { get; set; } 
+    {
+        Sku = sku;
+        InternalCode = internalCode;
+        Name = name;
+        Description = description;
+        CurrentUnitPrice = currentUnitPrice;
+        StockQuantity = stockQuantity;
+        IsAtive = isAtive;
+    }
+    public required string Sku { get; set; }
     public string? InternalCode { get; set; }
-    public string? Name { get; set; }
+    public required string Name { get; set; }
     public string? Description { get; set; }
-    public decimal CurrentUnitPrice { get; set; }
-    public int StockQuantity { get; set; }
+    public decimal CurrentUnitPrice
+    {
+        get => CurrentUnitPrice;
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException("El precio debe ser mayor a 0.");
+        }
+    }
+    public int StockQuantity
+    {
+        get => StockQuantity;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("La cantidad de stock no puede ser negativa.");
+        }
+    }
     public bool IsAtive { get; set; }
 
 
-    
+
     public ICollection<OrderItem>? Items { get; set; }
 }
